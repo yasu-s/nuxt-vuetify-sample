a00830b2359b68bbb8314c6c69a886d4226bc75f
Yasuyuki Saito <ya_saito@uluru.jp>
2022-05-16 14:29:44 +0900

    vuetify/vite-plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from 'nuxt'
+import vuetify from '@vuetify/vite-plugin'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -19,5 +20,6 @@ export default defineNuxtConfig({
     define: {
       'process.env.DEBUG': 'false',
     },
+    plugins: [vuetify({})],
   },
 })
