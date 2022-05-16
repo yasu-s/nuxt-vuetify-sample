db57f88d4df0ea88f35f8fc63d89acc45229b5b7
Yasuyuki Saito <ya_saito@uluru.jp>
2022-05-16 17:40:45 +0900

    修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,14 +12,29 @@ export default defineNuxtConfig({
     strict: true,
   },
   css: ['vuetify/lib/styles/main.sass'],
-  build: {
-    transpile: ['vuetify'],
-  },
+  // build: {
+  //   transpile: ['vuetify'],
+  // },
   modules: ['./modules/sample'],
   vite: {
     define: {
       'process.env.DEBUG': 'false',
     },
-    plugins: [vuetify({})],
+    plugins: [
+      vuetify({}),
+      {
+        // https://github.com/nuxt/framework/discussions/1183#discussioncomment-1997354
+        // https://github.com/nuxt/framework/issues/2798
+        configResolved(config): void {
+          const idx = config.plugins.findIndex((plugin) => plugin.name === 'vuetify:import')
+          const vueIdx = config.plugins.findIndex((plugin) => plugin.name === 'vite:vue')
+          if (~idx && idx < vueIdx) {
+            const plugin = config.plugins[idx]
+            config.plugins.splice(idx, 1)
+            config.plugins.splice(vueIdx, 0, plugin)
+          }
+        },
+      },
+    ],
   },
 })
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,13 +1,13 @@
 import { defineNuxtPlugin } from '#app'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin((app) => {
   // https://next.vuetifyjs.com/en/getting-started/installation/
   const vuetify = createVuetify({
-    components,
-    directives,
+    // components,
+    // directives,
     theme: {
       defaultTheme: 'myCustomTheme',
       themes: {
