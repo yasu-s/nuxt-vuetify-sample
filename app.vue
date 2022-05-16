6d67b1e37cc44a5fa6e1a723a982af33cb3b43eb
Yasuyuki Saito <ya_saito@uluru.jp>
2022-05-16 12:09:38 +0900

    bundle

--- app.vue
+++ app.vue
@@ -3,12 +3,12 @@ useHead({
   titleTemplate: 'My App - %s',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   charset: 'utf-8',
-  link: [
-    {
-      rel: 'stylesheet',
-      href: 'https://cdn.jsdelivr.net/npm/font-awesome@4.x/css/font-awesome.min.css',
-    },
-  ],
+  // link: [
+  //   {
+  //     rel: 'stylesheet',
+  //     href: 'https://cdn.jsdelivr.net/npm/font-awesome@4.x/css/font-awesome.min.css',
+  //   },
+  // ],
 })
 </script>
 
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,6 +3,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, fa } from 'vuetify/iconsets/fa4'
+import 'font-awesome/css/font-awesome.min.css'
 
 export default defineNuxtPlugin((app) => {
   // https://next.vuetifyjs.com/en/getting-started/installation/
