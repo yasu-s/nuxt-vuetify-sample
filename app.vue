3122d56972dd8c106a6fe08537381923f08886c9
Yasuyuki Saito <ya_saito@uluru.jp>
2022-05-16 11:44:31 +0900

    バンドル1

--- app.vue
+++ app.vue
@@ -3,13 +3,13 @@ useHead({
   titleTemplate: 'My App - %s',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   charset: 'utf-8',
-  link: [
-    {
-      // https://fonts.google.com/icons?selected=Material+Icons
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp',
-    },
-  ],
+  // link: [
+  //   {
+  //     // https://fonts.google.com/icons?selected=Material+Icons
+  //     rel: 'stylesheet',
+  //     href: 'https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp',
+  //   },
+  // ],
 })
 </script>
 
