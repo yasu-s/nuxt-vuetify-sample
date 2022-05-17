3839a06e8c67b40ef6d1f7084f79f9323b507e85
Yasuyuki Saito <ya_saito@uluru.jp>
2022-05-17 12:37:52 +0900

    dialog

--- pages/components.vue
+++ pages/components.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const snackbar = ref(false)
+const dialog = ref(false)
 </script>
 
 <template>
@@ -41,7 +42,28 @@ const snackbar = ref(false)
     </v-card>
     <v-card>
       <v-btn @click="snackbar = !snackbar">snackbar</v-btn>
+      <v-btn @click="dialog = true">dialog</v-btn>
     </v-card>
     <v-snackbar v-model="snackbar"> テスト </v-snackbar>
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2"> Privacy Policy </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
